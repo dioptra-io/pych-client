117199d1b2d1165e69c652ab3b7e060b5f22304a
Maxime Mouchet <max@maxmouchet.com>
2022-06-03 13:15:37 +0200

    add get_clickhouse_client_args

---
+++ pych_client/clickhouse_client.py
@@ -0,0 +1,21 @@
+from pych_client.typing import Params, Settings
+
+
+def get_clickhouse_client_args(
+    config: dict, query: str, params: Params = None, settings: Settings = None
+) -> list[str]:
+    """Return the equivalent arguments for the `clickhouse-client` command."""
+    args = [
+        f"--query='{query}'",
+        f"--database='{config['database']}'",
+        f"--user='{config['username']}'",
+    ]
+    if config["password"]:
+        args.append(f"--password='{config['password']}'")
+    if params:
+        args.extend(f"--param_{k}='{v}'" for k, v in params.items())
+    if settings:
+        if f := settings.pop("default_format", None):
+            settings["format"] = f
+        args.extend(f"--{k}='{v}'" for k, v in settings.items())
+    return args
---
+++ tests/test_clickhouse_client.py
@@ -0,0 +1,17 @@
+from pych_client.clickhouse_client import get_clickhouse_client_args
+
+
+def test_get_clickhouse_client_args(client):
+    args = get_clickhouse_client_args(
+        client.config,
+        "SELECT {val:UInt64}",
+        {"val": 1},
+        {"default_format": "JSONEachRow"},
+    )
+    assert args == [
+        "--query='SELECT {val:UInt64}'",
+        "--database='default'",
+        "--user='default'",
+        "--param_val='1'",
+        "--format='JSONEachRow'",
+    ]
